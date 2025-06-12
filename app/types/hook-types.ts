bddd06c0d4e5103dbc9c25e25f19911d2de95ade
Filip Nikolin <f.nikolin997@gmail.com>
2025-06-12 23:31:20 +0200

    Adapt hook-types to new changes in useRPSLS hook

--- app/types/hook-types.ts
+++ app/types/hook-types.ts
@@ -1,10 +1,11 @@
 export type UseRPSLS = {
   playerScore: number;
   computerScore: number;
-  gameResult: string;
+  gameResult: GameResultRemaped;
   choices: Choice[];
   playGame: (choice: number) => void;
   resetScore: () => void;
+  refetchChoices: () => void;
   loading?: boolean;
   error?: string | null;
 };
@@ -12,4 +13,20 @@ export type UseRPSLS = {
 export type Choice = {
   id: number;
   name: string;
+};
+
+export type GameResultAPIResponse = {
+  computer: number;
+  player: number;
+  results: string;
+};
+
+export type GameResultRemaped = {
+  playerChoiceName: string;
+  computerChoiceName: string;
+  playerEmoji: any;
+  computerEmoji: any;
+  computer: number;
+  player: number;
+  results: string;
 };
